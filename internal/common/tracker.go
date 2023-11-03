2cbfbd28b0df44d45fae1fbf50d6280d46f696bd
Rui Lopes <ruilopesm19@gmail.com>
2023-11-03 17:06:33 +0000

    refactor: packet structs (#10)
    Co-authored-by: chico ferreira <36338391+chicoferreira@users.noreply.github.com>

--- internal/common/tracker.go
+++ internal/common/tracker.go
@@ -11,9 +11,26 @@ const publishChunkType = 3
 const requestFileType = 4
 const answerNodesType = 5
 
+func PacketStructFromPacketType(packetType uint) interface{} {
+	switch packetType {
+	case publishFileType:
+		return PublishFilePacket{Type: publishFileType}
+	case alreadyExistsType:
+		return AlreadyExistsPacket{Type: alreadyExistsType}
+	case publishChunkType:
+		return PublishChunkPacket{Type: publishChunkType}
+	case requestFileType:
+		return RequestFilePacket{Type: requestFileType}
+	case answerNodesType:
+		return AnswerNodesPacket{Type: answerNodesType}
+	default:
+		return nil
+	}
+}
+
 // NODE -> TRACKER
 
-type PublishFile struct {
+type PublishFilePacket struct {
 	Type           uint8
 	NameSize       uint8
 	NumberOfChunks uint16
@@ -22,7 +39,7 @@ type PublishFile struct {
 	ChunkHashes    [][20]byte
 }
 
-func (pf *PublishFile) Create(name string, fileHash [20]byte, chunkHashes [][20]byte) {
+func (pf *PublishFilePacket) Create(name string, fileHash [20]byte, chunkHashes [][20]byte) {
 	pf.Type = uint8(publishFileType)
 	pf.NameSize = uint8(len(name))
 	pf.NumberOfChunks = uint16(len(chunkHashes))
@@ -31,15 +48,15 @@ func (pf *PublishFile) Create(name string, fileHash [20]byte, chunkHashes [][20]
 	pf.ChunkHashes = chunkHashes
 }
 
-func (pf *PublishFile) ReadString(reader *bytes.Reader) error {
+func (pf *PublishFilePacket) ReadString(reader *bytes.Reader) error {
 	return serialization.ReadStringCallback(reader, &pf.FileName, int(pf.NameSize))
 }
 
-func (pf *PublishFile) ReadSliceOfSliceByte20(reader *bytes.Reader) error {
+func (pf *PublishFilePacket) ReadSliceOfSliceByte20(reader *bytes.Reader) error {
 	return serialization.ReadSliceOfSliceByte20Callback(reader, &pf.ChunkHashes, int(pf.NumberOfChunks))
 }
 
-type PublishChunk struct {
+type PublishChunkPacket struct {
 	Type         uint8
 	BitfieldSize uint16
 	Reserved     uint8
@@ -47,7 +64,7 @@ type PublishChunk struct {
 	Bitfield     []byte
 }
 
-func (pc *PublishChunk) Create(fileHash [20]byte, bitfield []uint8) {
+func (pc *PublishChunkPacket) Create(fileHash [20]byte, bitfield []uint8) {
 	binaryBitField := serialization.EncodeBitField(bitfield)
 	bitfieldSize := len(binaryBitField)
 
@@ -58,49 +75,49 @@ func (pc *PublishChunk) Create(fileHash [20]byte, bitfield []uint8) {
 	pc.Bitfield = binaryBitField
 }
 
-func (pc *PublishChunk) ReadSliceByte(reader *bytes.Reader) error {
+func (pc *PublishChunkPacket) ReadSliceByte(reader *bytes.Reader) error {
 	return serialization.ReadSliceByteCallback(reader, &pc.Bitfield, int(pc.BitfieldSize))
 }
 
-type RequestFile struct {
+type RequestFilePacket struct {
 	Type     uint8
 	NameSize uint8
 	Reserved uint16
 	FileName string
 }
 
-func (rf *RequestFile) Create(fileName string) {
+func (rf *RequestFilePacket) Create(fileName string) {
 	rf.Type = uint8(requestFileType)
 	rf.NameSize = uint8(len(fileName))
 	rf.Reserved = uint16(0)
 	rf.FileName = fileName
 }
 
-func (rf *RequestFile) ReadString(reader *bytes.Reader) error {
+func (rf *RequestFilePacket) ReadString(reader *bytes.Reader) error {
 	return serialization.ReadStringCallback(reader, &rf.FileName, int(rf.NameSize))
 }
 
 // // TRACKER -> NODE
 
-type AlreadyExists struct {
+type AlreadyExistsPacket struct {
 	Type     uint8
 	NameSize uint8
 	Reserved uint16
 	FileName string
 }
 
-func (ae *AlreadyExists) Create(fileName string) {
+func (ae *AlreadyExistsPacket) Create(fileName string) {
 	ae.Type = uint8(alreadyExistsType)
 	ae.NameSize = uint8(len(fileName))
 	ae.Reserved = uint16(0)
 	ae.FileName = fileName
 }
 
-func (ae *AlreadyExists) ReadString(reader *bytes.Reader) error {
+func (ae *AlreadyExistsPacket) ReadString(reader *bytes.Reader) error {
 	return serialization.ReadStringCallback(reader, &ae.FileName, int(ae.NameSize))
 }
 
-type AnswerNodes struct {
+type AnswerNodesPacket struct {
 	Type           uint8
 	SequenceNumber uint8
 	BitfieldSize   uint16
@@ -110,7 +127,7 @@ type AnswerNodes struct {
 	Bitfield       []byte
 }
 
-func (an *AnswerNodes) Create(sequenceNumber uint8, nodeIdentifier [4]byte, nodePort uint16, bitfield []uint8) {
+func (an *AnswerNodesPacket) Create(sequenceNumber uint8, nodeIdentifier [4]byte, nodePort uint16, bitfield []uint8) {
 	binaryBitField := serialization.EncodeBitField(bitfield)
 	bitfieldSize := len(binaryBitField)
 
@@ -123,6 +140,6 @@ func (an *AnswerNodes) Create(sequenceNumber uint8, nodeIdentifier [4]byte, node
 	an.Bitfield = binaryBitField
 }
 
-func (an *AnswerNodes) ReadSliceByte(reader *bytes.Reader) error {
+func (an *AnswerNodesPacket) ReadSliceByte(reader *bytes.Reader) error {
 	return serialization.ReadSliceByteCallback(reader, &an.Bitfield, int(an.BitfieldSize))
 }
